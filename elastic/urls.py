7ace1cb9e299dd0eb93543e7290a80f16b4b3809
Baejb <bjb6478@naver.com>
2022-09-07 21:54:18 +0900

    상권 클릭시 url에 상권이름으로 바뀌게 함. 서비스코드 받아오는거 해야함

--- elastic/urls.py
+++ elastic/urls.py
@@ -6,11 +6,11 @@ urlpatterns = [
     path('test/', views.GraphJSON.as_view()), # 그래프
     path('gu/key/', views.FindByGu_key.as_view()), # 시군구명으로 상권영역
     path('gu/enter/', views.FindByGu_enter.as_view()), # 시군구명으로 상권영역
-    path('population/',  views.Population.as_view()), # 인구
-    path('sales/', views.Sales.as_view()), # 추정매출
-    path('store/', views.Store.as_view()), # 점포
-    path('store/change/', views.StoreChange.as_view()), # 상권변화지표
-    path('facilities/', views.Facilities.as_view()), # 집객시설
+    # path('population/',  views.Population.as_view()), # 인구
+    # path('sales/', views.Sales.as_view()), # 추정매출
+    # path('store/', views.Store.as_view()), # 점포
+    # path('store/change/', views.StoreChange.as_view()), # 상권변화지표
+    # path('facilities/', views.Facilities.as_view()), # 집객시설
     path('index/', views.index),
     path('index/polygon/', views.polygon),
     path('index/polygon2/', views.polygon2),
